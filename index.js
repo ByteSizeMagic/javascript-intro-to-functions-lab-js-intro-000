949cf51c3bd281f8da00be8b659cb6099e656520
Crystal Schooling <the.thunderhawke@gmail.com>
2019-06-24 00:27:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ function logWhisper (string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma (string) {
+function sayHiToGrandma (string) 
+{
   if (string === string.toLowerCase())
+  {
     return "I can't hear you!";
-} else if (string === string.toUpperCase()) {
+} 
+  else if (string === string.toUpperCase()) 
+{
     return "YES INDEED";
-} else if (string === "I love you, Grandma.") {
+} 
+  else if (string === "I love you, Grandma.") 
+{
     return "I love you, too.";
 }
 }
